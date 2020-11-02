1ad0c7c85713c2c56bc63bc75951ff5e032ca001
Kirill <Kiril@PROMETHEUS>
2020-11-02 14:25:56 +0200

    1

--- TimeTable/TimeTable/TimeTable2.xaml.cs
+++ TimeTable/TimeTable/TimeTable2.xaml.cs
@@ -14,7 +14,7 @@ namespace TimeTable
         Label Tund1, Tund2, Tund3, Tund4, Tund5, Tund6, Tund7, Tund8, Esmaspaev, Teisipaev, Kolmapaev, Neljapaev, Reede, urok1, urok2, urok3, urok4, urok5, urok6, urok7, urok8, urok9, urok10, urok11, urok12, urok13, urok14;
 
 
-        //Frame frm, frm2;
+        
         public TimeTable2()
         {
             Grid abs = new Grid();
@@ -99,98 +99,98 @@ namespace TimeTable
             tap1.Tapped += async (s, e) =>
             {
                 urok1 = (Label)s;
-                await DisplayAlert("Доп. инфо", "Кабинет B221 \n Mihhailova Ljudmilla", "Kinni");
+                await DisplayAlert("Доп. инфо", " B221 \n Mihhailova Ljudmilla", "Kinni");
             };
             urok1.GestureRecognizers.Add(tap1);
             var tap2 = new TapGestureRecognizer();
             tap2.Tapped += async (s, e) =>
             {
                 urok2 = (Label)s;
-                await DisplayAlert("Доп. инфо", "Кабинет A123 \n Agapov Mikhail", "Kinni");
+                await DisplayAlert("Доп. инфо", " A123 \n Agapov Mikhail", "Kinni");
             };
             urok2.GestureRecognizers.Add(tap2);
             var tap3 = new TapGestureRecognizer();
             tap3.Tapped += async (s, e) =>
             {
                 urok3 = (Label)s;
-                await DisplayAlert("Доп. инфо", "Кабинет E07 \n Oleinik Marina", "Kinni");
+                await DisplayAlert("Доп. инфо", " E007 \n Oleinik Marina", "Kinni");
             };
             urok3.GestureRecognizers.Add(tap3);
             var tap4 = new TapGestureRecognizer();
             tap4.Tapped += async (s, e) =>
             {
                 urok4 = (Label)s;
-                await DisplayAlert("Доп. инфо", "Кабинет B002 \n Krull Jaanus", "Kinni");
+                await DisplayAlert("Доп. инфо", " B002 \n Krull Jaanus", "Kinni");
             };
             urok4.GestureRecognizers.Add(tap4);
             var tap5 = new TapGestureRecognizer();
             tap5.Tapped += async (s, e) =>
             {
                 urok5 = (Label)s;
-                await DisplayAlert("Доп. инфо", "Кабинет B242 \n Voronetskaja Julia", "Kinni");
+                await DisplayAlert("Доп. инфо", " B242 \n Voronetskaja Julia", "Kinni");
             };
             urok5.GestureRecognizers.Add(tap5);
             var tap6 = new TapGestureRecognizer();
             tap6.Tapped += async (s, e) =>
             {
                 urok6 = (Label)s;
-                await DisplayAlert("Доп. инфо", "Кабинет B236 \n Laaneväli-Toots Alina", "Kinni");
+                await DisplayAlert("Доп. инфо", " B236 \n Laaneväli-Toots Alina", "Kinni");
             };
             urok6.GestureRecognizers.Add(tap6);
             var tap7 = new TapGestureRecognizer();
             tap7.Tapped += async (s, e) =>
             {
                 urok7 = (Label)s;
-                await DisplayAlert("Доп. инфо", "Кабинет A243 \n Shkarbanova Lury", "Kinni");
+                await DisplayAlert("Доп. инфо", " A243 \n Shkarbanova Lury", "Kinni");
             };
             urok7.GestureRecognizers.Add(tap7);
             var tap8 = new TapGestureRecognizer();
             tap8.Tapped += async (s, e) =>
             {
                 urok8 = (Label)s;
-                await DisplayAlert("Доп. инфо", "Кабинет B002 \n Krull Jaanus", "Kinni");
+                await DisplayAlert("Доп. инфо", " B002 \n Krull Jaanus", "Kinni");
             };
             urok8.GestureRecognizers.Add(tap8);
             var tap9 = new TapGestureRecognizer();
             tap9.Tapped += async (s, e) =>
             {
                 urok9 = (Label)s;
-                await DisplayAlert("Доп. инфо", "Кабинет B144 \n Pesetskaja Svetlana", "Kinni");
+                await DisplayAlert("Доп. инфо", " B144 \n Pesetskaja Svetlana", "Kinni");
             };
             urok9.GestureRecognizers.Add(tap9);
             var tap10 = new TapGestureRecognizer();
             tap10.Tapped += async (s, e) =>
             {
                 urok10 = (Label)s;
-                await DisplayAlert("Доп. инфо", "Кабинет a243 \n Shkarbanova Lury", "Kinni");
+                await DisplayAlert("Доп. инфо", " a243 \n Shkarbanova Lury", "Kinni");
             };
             urok10.GestureRecognizers.Add(tap10);
             var tap11 = new TapGestureRecognizer();
             tap11.Tapped += async (s, e) =>
             {
                 urok11 = (Label)s;
-                await DisplayAlert("Доп. инфо", "Кабинет A123 \n Agapov Mikhail", "Kinni");
+                await DisplayAlert("Доп. инфо", " A123 \n Agapov Mikhail", "Kinni");
             };
             urok11.GestureRecognizers.Add(tap11);
             var tap12 = new TapGestureRecognizer();
             tap12.Tapped += async (s, e) =>
             {
                 urok12 = (Label)s;
-                await DisplayAlert("Доп. инфо", "Кабинет B242 \n Voronetskaja Julia", "Kinni");
+                await DisplayAlert("Доп. инфо", " B242 \n Voronetskaja Julia", "Kinni");
             };
             urok12.GestureRecognizers.Add(tap12);
             var tap13 = new TapGestureRecognizer();
             tap13.Tapped += async (s, e) =>
             {
                 urok13 = (Label)s;
-                await DisplayAlert("Доп. инфо", "Кабинет B144 \n Pesetskaja Svetlana", "Kinni");
+                await DisplayAlert("Доп. инфо", " B144 \n Pesetskaja Svetlana", "Kinni");
             };
             urok13.GestureRecognizers.Add(tap13);
             var tap14 = new TapGestureRecognizer();
             tap14.Tapped += async (s, e) =>
             {
                 urok14 = (Label)s;
-                await DisplayAlert("Доп. инфо", "Кабинет E007 \n Oleinik Marina", "Kinni");
+                await DisplayAlert("Доп. инфо", " E007 \n Oleinik Marina", "Kinni");
             };
             urok14.GestureRecognizers.Add(tap14);
         }
